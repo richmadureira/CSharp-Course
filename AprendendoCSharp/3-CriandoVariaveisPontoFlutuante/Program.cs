21ea418d0657bdbe0d0e0cfa2e27b8686eb601c4
Richard <madureira.rm@outlook.com>
2019-03-11 22:27:38 -0300

    Incluindo aulas

---
+++ AprendendoCSharp/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 3 Criando Variaveis ponto flutuante");
+
+            int salario;
+            salario = 1200;
+
+            Console.WriteLine(salario);
+
+            double idade;
+            idade = 5.0 / 3;
+            Console.WriteLine("5 / 3 = " + idade);
+
+            Console.WriteLine(salario * idade);
+
+            Console.WriteLine("A execução acabou. Tecle Enter para sair. . .");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ AprendendoCSharp/aliquotaifs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliquotaifs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ex aliquotas com uso de IFs");
+
+            double Salario = 3300.00;
+
+            if (Salario > 1900 && Salario <= 2800)
+            {
+                Salario = Salario - 142;
+            }
+            
+            if (Salario > 2800 && Salario <= 3751)
+            {
+                Salario = Salario - 350;
+            }
+            
+            if (Salario > 3751.01 && Salario <= 4664)
+            {
+                Salario = Salario - 636;
+            }
+
+            Console.WriteLine(Salario);
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
