d5cb7f20c597899c52c6a9f854c7062cb1e4aeaf
richmadureira <madureira.rm@outlook.com>
2019-03-13 13:39:29 -0300

    Parte 2

---
+++ csharppoo-aula2/04-ByteBank/ContaCorrente.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+public class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int numero;
+    public double saldo = 100;
+
+    public bool Sacar(double valor)
+    {
+        if(saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            return true;
+        }
+    }
+}
+
+
---
+++ csharppoo-aula2/04-ByteBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contadoBruno = new ContaCorrente();
+
+            contadoBruno.titular = "Bruno";
+
+            Console.WriteLine(contadoBruno.saldo);
+
+            bool resultadoSaque = contadoBruno.Sacar(50);
+
+            Console.WriteLine(contadoBruno.saldo);
+            Console.WriteLine(resultadoSaque);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
