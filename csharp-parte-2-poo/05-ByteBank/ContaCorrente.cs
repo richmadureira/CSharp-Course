4b729a3196aef5851f306ec2cb4409751f9a3fb4
richmadureira <madureira.rm@outlook.com>
2019-03-20 14:49:04 -0300

    Aula 4 POO

---
+++ csharp-parte-2-poo/05-ByteBank/ContaCorrente.cs
@@ -0,0 +1,42 @@
+﻿
+
+
+public class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int numero;
+    public double saldo = 100;
+
+    public bool Sacar(double valor)
+    {
+        if(saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            return true;
+        }
+    }
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;       
+    }
+
+    public bool Transferir(double valor, ContaCorrente contaDestino)
+    {
+        if (saldo < valor){
+            return false;
+        }
+
+        saldo -= valor;
+        contaDestino.Depositar(valor);
+        return true;
+        
+    }
+}
+
+
---
+++ csharp-parte-2-poo/05-ByteBank/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contadoBruno = new ContaCorrente();
+
+            contadoBruno.titular = "Bruno";
+
+            Console.WriteLine(contadoBruno.saldo);
+
+            bool resultadoSaque = contadoBruno.Sacar(50);
+
+            Console.WriteLine(contadoBruno.saldo);
+            Console.WriteLine(resultadoSaque);
+
+            contadoBruno.Depositar(500);
+            Console.WriteLine(contadoBruno.saldo);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+
+            Console.WriteLine("Saldo do Bruno: " + contadoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela.saldo);
+
+            contadoBruno.Transferir(200, contaDaGabriela);
+            Console.WriteLine("Foi transferido 200 da Conta do Bruno pra Gabriela ");
+            Console.WriteLine("Saldo do Bruno: " + contadoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela.saldo);
+
+            Console.ReadLine();
+
+        }
+    }
+}
