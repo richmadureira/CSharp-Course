95e61922507f00a5b7d6f111b2ce799048bf96ef
richmadureira <madureira.rm@outlook.com>
2019-03-25 13:41:39 -0300

    atualizacao aula 5

---
+++ csharp-parte-2-poo/06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,80 @@
+﻿using _06_ByteBank;
+
+namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+
+        public Cliente Titular {get; set;}
+
+
+        public int agencia;
+        public int numero;
+        private double _saldo;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        //public void SetSaldo(double saldo)
+        //{
+        //    if(saldo < 0)
+        //    {
+        //        return;
+        //    }
+          
+        //    this.saldo = saldo;          
+
+        //}
+
+        //public double GetSaldo()
+        //{
+        //    return (saldo);
+        //}
+
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+
+            this._saldo -= valor;
+            return true;
+
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+
+            this._saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+
+        }
+
+        
+    }
+}
---
+++ csharp-parte-2-poo/06-ByteBank/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.nome = "Richard";
+            cliente.cpf = "345.432.234-45";
+            cliente.profissao = "Desenvolvedor";
+
+
+            conta.Titular = cliente;
+            conta.Saldo = 10;
+            Console.WriteLine(conta.Titular.nome);
+            Console.WriteLine(conta.Saldo);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
