2a7741442261e4209027afb494f7ad4b25200446
richmadureira <madureira.rm@outlook.com>
2019-04-02 13:29:51 -0300

    Atualizacao Parte 3

---
+++ csharp-parte-3-poo3/ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    class Funcionario
+    {
+        private int _tipo;
+
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int tipo)
+        {
+             _tipo = tipo;
+        }
+
+        public double GetBonificacao()
+        {
+            if(_tipo == 1)
+            return Salario;
+
+            return Salario * 0.10;
+        }
+    }
+}
---
+++ csharp-parte-3-poo3/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario Carlos = new Funcionario(1);
+
+            Carlos.Nome = "Carlos";
+            Carlos.CPF = "457.818.708-06";
+            Carlos.Salario = 2000;
+
+
+            Console.WriteLine(Carlos.Nome);
+            Console.WriteLine(Carlos.GetBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
