c2999e51cccee5ba0fd20adde8c72af2053da463
richmadureira <madureira.rm@outlook.com>
2019-04-08 13:05:54 -0300

    Atualizacao ultima aula 01 parte 03

--- csharp-parte-3-poo3/ByteBank/ByteBank/Program.cs
+++ csharp-parte-3-poo3/ByteBank/ByteBank/Program.cs
@@ -11,15 +11,23 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            Funcionario Carlos = new Funcionario(1);
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
 
-            Carlos.Nome = "Carlos";
-            Carlos.CPF = "457.818.708-06";
-            Carlos.Salario = 2000;
+            Funcionario richard = new Funcionario();
+            richard.Nome = "Richard";
+            richard.CPF = "457.818.708-06";
+            richard.Salario = 2000;
 
+            Diretor alex = new Diretor();
+            alex.Nome = "Alex";
+            alex.CPF = "283.832.293-03";
+            alex.Salario = 5000;
 
-            Console.WriteLine(Carlos.Nome);
-            Console.WriteLine(Carlos.GetBonificacao());
+            gerenciador.Registrar(richard);
+            gerenciador.Registrar(alex);
+
+            Console.WriteLine("Total de Bonificacao: " + gerenciador.GetBonificacao());
+            
 
             Console.ReadLine();
         }
