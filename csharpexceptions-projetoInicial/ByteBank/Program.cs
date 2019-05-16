9a93480f2b64f77e519740707dda5cdcd4354261
richmadureira <madureira.rm@outlook.com>
2019-05-16 13:35:23 -0300

    Atualização aula 1 Excessões

--- csharpexceptions-projetoInicial/ByteBank/Program.cs
+++ csharpexceptions-projetoInicial/ByteBank/Program.cs
@@ -10,7 +10,51 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Metodo();                       
+            }
+            catch (DivideByZeroException erro)
+            {
+                Console.WriteLine("Não é possivel divisão por 0.");
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Aconteceu um erro!");
+            }                     
+
             Console.ReadLine();
         }
+
+        // Teste com a cadeia de chamada:
+        // Metodo -> TestaDivisao -> Dividir
+
+        static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + "é" + resultado);
+        }
+
+        public static int Dividir(int Numero, int divisor)
+        {
+            ContaCorrente conta = null;
+            //Console.WriteLine(conta.Saldo);
+
+            return Numero / divisor;
+        }
+
+
+
+        
+
+        
+
     }
 }
