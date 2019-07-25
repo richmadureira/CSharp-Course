0bb42640c27b755e85d3a1a376f3edc6dcd7752e
richmadureira <madureira.rm@outlook.com>
2019-07-25 12:55:10 -0300

    mudando diretorio e inserindo projeto inicial pt5

--- Alura-CSharpPT4_csharpexceptions/ByteBank/ContaCorrente.cs
+++ Alura-CSharpPT4_csharpexceptions/ByteBank/ContaCorrente.cs
@@ -1,35 +1,22 @@
-﻿// using _05_ByteBank;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ByteBank
 {
     public class ContaCorrente
     {
-        public Cliente Titular { get; set; }
-
+        public static double TaxaOperacao { get; private set; }
         public static int TotalDeContasCriadas { get; private set; }
-
+        public Cliente Titular { get; set; }
+        public int Numero { get; }
+        public int Agencia { get; }
 
         private int _agencia;
-        public int Agencia
-        {
-            get
-            {
-                return _agencia;
-            }
-            set
-            {
-                if (value <= 0)
-                {
-                    return;
-                }
-
-                _agencia = value;
-            }
-        }
-        public int Numero { get; set; }
-
+        
         private double _saldo = 100;
-
         public double Saldo
         {
             get
@@ -47,15 +34,24 @@ namespace ByteBank
             }
         }
 
-
         public ContaCorrente(int agencia, int numero)
         {
+            if (agencia <= 0)
+            {
+                throw new ArgumentException("O argumento agencia deve ser maior que 0.", nameof(agencia));
+            }
+
+            if (numero <= 0)
+            {
+                throw new ArgumentException("O argumento numero deve ser maior que 0.", nameof(numero));
+            }
+
             Agencia = agencia;
             Numero = numero;
 
             TotalDeContasCriadas++;
+            TaxaOperacao = 30 / TotalDeContasCriadas;
         }
-
 
         public bool Sacar(double valor)
         {
@@ -72,7 +68,6 @@ namespace ByteBank
         {
             _saldo += valor;
         }
-
 
         public bool Transferir(double valor, ContaCorrente contaDestino)
         {
