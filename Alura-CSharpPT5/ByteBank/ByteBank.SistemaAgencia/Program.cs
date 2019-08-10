f3b29ecea910c0c2035b6421209f2173d7a4daec
Richard <madureira.rm@outlook.com>
2019-08-10 15:08:07 -0300

    Atualização Packages Humanizer

--- Alura-CSharpPT5/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ Alura-CSharpPT5/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Humanizer;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -11,8 +12,21 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(374, 3627362);
-            Console.WriteLine(conta.Saldo);
+            //ContaCorrente conta = new ContaCorrente(374, 3627362);
+            //Console.WriteLine(conta.Saldo);
+
+            DateTime dataFimpPagamento = new DateTime(2018, 8, 17);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = dataFimpPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(dataFimpPagamento);
+            Console.WriteLine(dataCorrente);
+            Console.WriteLine(diferenca);
+
+            Console.WriteLine(mensagem);
 
             Console.ReadLine();
         }
