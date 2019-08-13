cd4ad51e5fb147df1c37dae67c86f27cbda1a258
richmadureira <madureira.rm@outlook.com>
2019-08-13 13:19:30 -0300

    .

---
+++ Alura-CSharpPT6/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "pagina?argumentos";
+
+            int indiceInterrogacao = url.IndexOf('?');
+
+            //Console.WriteLine(indiceInterrogacao);
+
+            string argumentos = url.Substring(indiceInterrogacao+1);
+            Console.WriteLine(argumentos);
+
+            Console.ReadLine();
+        }
+    }
+}
